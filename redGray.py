f8caa42b65e41e6ce5edecb2a6f4b8053579a40a
yair <aythb@hotmail.com>
2022-01-04 23:36:04 -0600

    se agrega la deteccion del color rojo en la imagen

--- redGray.py
+++ redGray.py
@@ -2,15 +2,23 @@ import cv2
 import numpy as np
 import imutils
 
+#se crean los rangos para la deteccion de color
 rojoBajo1 = np.array([0,140,90], np.uint8)
 rojoAlto1 = np.array([8,255,255], np.uint8)
 rojoBajo2 = np.array([160,140,90], np.uint8)
 rojoAlto2 = np.array([180,255,255], np.uint8)
 
+#se lee la imagen a tratar y se ajustan los tamaños
 imagen = cv2.imread('img.jpeg')
 imagen = imutils.resize(imagen, width=640)
 
+#se transforma la imagen original en los distintos formatos a tratar
 imagenGris = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 imagenGris = cv2.cvtColor(imagenGris, cv2.COLOR_GRAY2BGR)
 imagenHSV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
 
+#detectamos el color rojo con los rangos especificos
+maskRojo1 = cv2.inRange(imagenHSV,rojoBajo1,rojoAlto1)
+maskRojo2 = cv2.inRange(imagenHSV,rojoBajo2,rojoAlto2)
+mask = cv2.add(maskRojo1,maskRojo2)
+mask = cv2.medianBlur(mask, 7)
